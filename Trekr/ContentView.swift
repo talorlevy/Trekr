8d7a6adc256e88c36a869bc3ec64497ed84c9d94
Talor Levy <talorlv123@hotmail.com>
2023-04-17 09:45:26 -0700

    Initial Commit

---
+++ Trekr/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Trekr
+//
+//  Created by Talor Levy on 4/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Talor Levy on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

865330af59f34b431f63f961c250eb6509cdc36e
Talor Levy <talorlv123@hotmail.com>
2023-04-17 12:34:08 -0700

    Added image and text to ContentView and downloaded assets from link on YouTube video

--- Trekr/ContentView.swift
+++ Trekr/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("highlands")
+                .resizable()
+                .scaledToFit()
+            
+            Text("The Highlands")
+                .font(.largeTitle)
+                .bold()
         }
-        .padding()
     }
 }
 
